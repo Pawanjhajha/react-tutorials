3715ce7b42351930f50475a91aa0e689ddc75374
pawanjhajha <pawankumarjhajharia3@gmail.com>
2024-10-11 07:49:29 +0530

    create the compnets

---
+++ src/app.js
@@ -0,0 +1,7 @@
+ function App(){
+    return <h1>Hello this is my app componet</h1>//this is jsx .very componet need to return jsx exprestion
+}
+export default App;
+
+//default means export the as default ex:import App form './app';
+//of export only user ex:import {App} from App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDom from "react-dom/client";
+import App from "./app";//in react app we dont need the specify extion but in node we have to specify .js extention
 
-const header=<h1>This is my first react app</h1>//this is jsx code react.createElement() automatically call bihind the seen and pass into babel and then balbel convert jsx into plain js
-console.log(header,"header")//still it will show in virtual dom 
-ReactDom.render(header,document.getElementById('root'));//this will convert the virtual dom to real dom/browser dom
+
+// const header=<h1>This is my first react app</h1>
+// ReactDom.render(header,document.getElementById('root')); in the react 18 the ReactDom.render() method is deprecated so insteted of ReactDom.render() 
+//we will use ReactDom.createRoot() method
+
+const root=ReactDom.createRoot(document.getElementById('root'))//it will take the container inside the  container append all the element,
+root.render(<App/>)//inside the <div id="root"> container it will render our componet
