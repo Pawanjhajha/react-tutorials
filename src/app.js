8ff2cca4cb7ebe5990b808a88390428a6c557a00
pawanjhajha <pawankumarjhajharia3@gmail.com>
2024-10-11 08:18:22 +0530

    write the jsx

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
  function App(){
-    return <h1>Hello this is my app componet</h1>//this is jsx .very componet need to return jsx exprestion
+    //usually we can`t write html in js file. so we have to write jsx .if we want to write html in js file then we have to wraap the html into duble quotes("")
+    //babel will convert jsx onto plain js.because the browser dont understand the jsx
+    
+    // const plainJs=document.createElement("<h1>")
+    // console.log(plainJs)
+    // return plainJs;
+
+    //advantage of jsx 
+    //1.jsx give the advantage to create uI with html
+    //2. in jsx we can write the expresion on html ex. return <h1>This is my age:{15+10}</h1>,
+    // const message="pawan"
+    // return <h1>hello mr:{message}</h1>
+    return (
+        <>
+        <h1>Hello</h1>
+        <p>Pawan</p>
+        </>
+    )
 }
 export default App;
-
-//default means export the as default ex:import App form './app';
-//of export only user ex:import {App} from App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import App from "./app";//in react app we dont need the specify extion but in node we have to specify .js extention
+import App from "./app";
 
 
-// const header=<h1>This is my first react app</h1>
-// ReactDom.render(header,document.getElementById('root')); in the react 18 the ReactDom.render() method is deprecated so insteted of ReactDom.render() 
-//we will use ReactDom.createRoot() method
+const root=ReactDom.createRoot(document.getElementById('root'))
+root.render(<App/>)
 
-const root=ReactDom.createRoot(document.getElementById('root'))//it will take the container inside the  container append all the element,
-root.render(<App/>)//inside the <div id="root"> container it will render our componet
+// let div=document.getElementById('root')
+// div.append(App())
