2063c6c6b6acceea2b79845cfbe20e0881cd75e4
pawanjhajha <pawankumarjhajharia3@gmail.com>
2024-10-14 07:36:35 +0530

    adding the child componet

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
  import './app.css'
- //import the css file
+import Product from './componets/product/product';
  function App(){
-//in react we have to use className for using css.becauseinternally this jsx convert to plain js and js class is a reserved keyword
     return (
        <>
        <h1 className="header">This is my Header</h1>
        <p className='paragraph'>this is my first css componet</p>
+       <Product></Product>
        </>
     )
 }
---
+++ src/componets/product/product.js
@@ -0,0 +1,10 @@
+function Product(){
+    //first letter of componet name should be capital
+    return(
+        <>
+        <h1>All Products</h1>
+        </>
+    )
+}
+
+export default Product;
