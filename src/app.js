ee7281423631ef50409fef7fa18d9a93feefa3f7
pawanjhajha <pawankumarjhajharia3@gmail.com>
2024-10-14 07:28:25 +0530

    add the css to componet

--- src/app.js
+++ src/app.js
@@ -1,22 +1,12 @@
+ import './app.css'
+ //import the css file
  function App(){
-    //usually we can`t write html in js file. so we have to write jsx .if we want to write html in js file then we have to wraap the html into duble quotes("")
-    //babel will convert jsx onto plain js.because the browser dont understand the jsx
-    
-    // let plainJs=document.createElement("h1")
-    // // console.log(plainJs)
-    // plainJs.textContent="hello pawan";
-    // return plainJs;
-
-    //advantage of jsx 
-    //1.jsx give the advantage to create uI with html
-    //2. in jsx we can write the expresion on html ex. return <h1>This is my age:{15+10}</h1>,
-    // const message="pawan"
-    // return <h1>hello mr:{message}</h1>
+//in react we have to use className for using css.becauseinternally this jsx convert to plain js and js class is a reserved keyword
     return (
-        <>
-        <h1>Hello</h1>
-        <p>Pawan</p>
-        </>
+       <>
+       <h1 className="header">This is my Header</h1>
+       <p className='paragraph'>this is my first css componet</p>
+       </>
     )
 }
 export default App;
