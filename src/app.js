acdf5cb0894841ef1826aacb2f24fce620922e33
pawanjhajha <pawankumarjhajharia3@gmail.com>
2024-10-15 07:42:22 +0530

    add the bootstrap andgobla css in react

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@
 import Product from './componets/product/product';
  function App(){
     return (
-       <>
-       <h1 className="header">This is my Header</h1>
-       <p className='paragraph'>this is my first css componet</p>
-       <Product></Product>
-       </>
+      <div className='container'>
+         <div className='row'>
+            <div className='col-lg-8 mx-auto'>
+               {/* mx-auto means margin form x-x auto*/}
+               <Product></Product>
+            </div>
+         </div>
+      </div>
     )
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import App from "./app";
-
+//add the bootstrap ex:- npm install bootstrap
+import 'bootstrap/dist/css/bootstrap.css';//add the bootstrap
+//if we want to goble css that will apply on hole react application so that will add on index.css that will apply on hole react application gobally
+import './index.css';
 
 const root=ReactDom.createRoot(document.getElementById('root'))
 root.render(<App/>)
 
-// let div=document.getElementById('root')
-// const appData=App();
-// console.log(appData,"appData")
-// div.append(appData)
