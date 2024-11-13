c04d0795607c2e34e5878e8ed2bd3007dbd1ab14
pawanjhajha <pawankumarjhajharia3@gmail.com>
2024-11-13 07:34:45 +0530

    add the event handler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  function showMsg(e){
+    console.log(e,"event")
+    alert('Button is clicked')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hiii
-        </a>
-      </header>
-    </div>
+    <>
+    <button className='btn btn-primary' onClick={showMsg}>click me</button>
+    </>
   );
 }
 
