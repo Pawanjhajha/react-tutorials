45916e0d420964c987767f23954157ef505b59d4
pawanjhajha <pawankumarjhajharia3@gmail.com>
2024-12-05 07:50:47 +0530

    create the useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
 
 function App() {
+  let[resourcType,SetResourceType]=useState('Home');
+  console.log("app componet.....")
+  useEffect(()=>{
+    console.log("useEffect execute......")
+  },[resourcType])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hiii
-        </a>
-      </header>
+      <button onClick={()=>{SetResourceType('Home')}}>Home</button>
+      <button onClick={()=>{SetResourceType("About")}}>About</button>
+      <button onClick={()=>{SetResourceType("Contact")}}>Contact</button>
+      <h3>{resourcType}</h3>
     </div>
   );
 }
