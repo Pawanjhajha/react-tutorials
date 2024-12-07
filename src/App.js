06402802be808c28483668cc98c22e24bafc2ad4
pawanjhajha <pawankumarjhajharia3@gmail.com>
2024-12-07 19:43:25 +0530

    set the value in local storage

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+// import Login from './login';
 
 function App() {
+  let[isLoggnIn,updateIsLoggin]=useState(false);
+  const isLogin=localStorage.getItem('isLoggedIn');
+  useEffect(()=>{
+    if(isLogin==='1'){
+      updateIsLoggin(true);
+    }
+  },[isLoggnIn])
+  console.log(isLogin,"isLogin")
+  const loginHandler=(email,password)=>{
+    localStorage.setItem('isLoggedIn','1')
+    updateIsLoggin(true);
+  }
+  const logOutHandler=()=>{
+    localStorage.removeItem('isLoggedIn');
+    updateIsLoggin(false);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hiii
-        </a>
-      </header>
-    </div>
+   <>
+    {!isLoggnIn && <h2>Not login</h2>}
+    <button onClick={loginHandler}>click to login</button>
+    <button onClick={logOutHandler}>click to logOut</button>
+   </>
   );
 }
 
